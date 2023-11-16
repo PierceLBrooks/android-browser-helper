5351033eea3d672f454cf2a67daaf903f17cf7c3
Pierce Brooks <piercebrooks@rocketmail.com>
2023-11-15 23:12:18 -0500

    experimenting with a secure custom permission model for extra web tab intent headers

--- demos/custom-tabs-headers/src/main/java/com/google/androidbrowserhelper/demos/customtabsheaders/MainActivity.java
+++ demos/custom-tabs-headers/src/main/java/com/google/androidbrowserhelper/demos/customtabsheaders/MainActivity.java
@@ -16,6 +16,8 @@
 
 package com.google.androidbrowserhelper.demos.customtabsheaders;
 
+//import com.google.androidbrowserhelper.demos.customtabsheaders.pack.R;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.browser.customtabs.CustomTabsCallback;
@@ -24,12 +26,21 @@ import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.browser.customtabs.CustomTabsService;
 import androidx.browser.customtabs.CustomTabsServiceConnection;
 import androidx.browser.customtabs.CustomTabsSession;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Browser;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -37,22 +48,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String ACTION = Browser.EXTRA_HEADERS + ".action";
+    private static String PERMISSION = "moe.haruue.test.permission.TEST";
     // This project is using a demo page hosted at Glitch. However, the demo will only work if the
     // Digital Asset Links validation is successful. So, when testing on your computer, remix the
     // project at https://glitch.com/edit/#!/custom-tabs-custom-he, edit the file under
     // `public/.well-known/assetlinks.json` with your own SHA-256 fingerprint (use Tools > Terminal
     // to find and edit the file), and update the URL below to the new project.
-    private static final Uri URL = Uri.parse("https://custom-tabs-custom-he.glitch.me/");
+    private static final Uri URL = Uri.parse("https://www.cylog.org/headers/");
 
-    private CustomTabsSession mSession;
-    private CustomTabsServiceConnection mConnection;
+    private CustomTabsSession mSession = null;
+    private CustomTabsServiceConnection mConnection = null;
 
-    private Button mExtraButton;
+    private Button mExtraButton = null;
+
+    private IntentFilter mIntentFilter = null;
+
+    private BroadcastReceiver mReceiver = null;
+
+    private Bundle mHeaders = null;
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        /*
         // Set up a callback that launches the intent after session was validated.
         CustomTabsCallback callback = new CustomTabsCallback() {
             @Override
@@ -87,7 +108,10 @@ public class MainActivity extends Activity {
                 "com.chrome.canary",
                 "com.chrome.dev",
                 "com.chrome.beta",
-                "com.android.chrome"
+                "com.android.chrome",
+                "org.mozilla.firefox",
+                "org.mozilla.focus",
+                "org.mozilla.focus.debug"
         );
         String packageName =
                 CustomTabsClient.getPackageName(MainActivity.this, packageNames, false);
@@ -96,8 +120,10 @@ public class MainActivity extends Activity {
                     .show();
         } else {
             // Bind the custom tabs service connection.
+            Log.i(TAG, packageName);
             CustomTabsClient.bindCustomTabsService(this, packageName, mConnection);
         }
+        */
 
     }
 
@@ -106,11 +132,47 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (PERMISSION.isEmpty()) {
+            PERMISSION += getString(R.string.permission_name);
+        }
+
         mExtraButton = findViewById(R.id.btn_extra);
+        //mExtraButton.setEnabled(false);
         mExtraButton.setOnClickListener(view -> {
             CustomTabsIntent intent = constructExtraHeadersIntent(mSession);
-            intent.launchUrl(MainActivity.this, URL);
+            if (intent != null) {
+                intent.launchUrl(MainActivity.this, URL);
+            }
         });
+
+        mHeaders = new Bundle();
+        mHeaders.putString("redirect-url", "https://www.google.com/");
+        mHeaders.putString("foo", "bar");
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(ACTION);
+        mIntentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent != null) {
+                    Log.i(TAG, "INTENT = " + intent.getAction());
+                    if (ACTION.equals(intent.getAction())) {
+                        Intent broadcastIntent = new Intent();
+                        broadcastIntent.setAction(Browser.EXTRA_HEADERS + ".response");
+                        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                        broadcastIntent.putExtra(Browser.EXTRA_HEADERS, mHeaders);
+                        sendBroadcast(broadcastIntent, PERMISSION);
+                        if (mReceiver != null) {
+                            unregisterReceiver(mReceiver);
+                            mReceiver = null;
+                        }
+                    }
+                }
+            }
+        };
+        ContextCompat.registerReceiver(this, mReceiver, mIntentFilter, ContextCompat.RECEIVER_EXPORTED);
     }
 
     @Override
@@ -123,17 +185,65 @@ public class MainActivity extends Activity {
             mConnection = null;
             mSession = null;
         }
-        mExtraButton.setEnabled(false);
+        //mExtraButton.setEnabled(false);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+            mReceiver = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.i(TAG, "PERMISSION RESULT = " + PERMISSION);
+        if (requestCode == 0) {
+            Log.i(TAG, "PERMISSION CODE = " + requestCode);
+            if (permissions.length == grantResults.length) {
+                for (int idx = 0; idx < permissions.length; ++idx) {
+                    Log.i(TAG, "PERMISSION = " + permissions[idx]);
+                    if (PERMISSION.equals(permissions[idx]) && grantResults[idx] == PackageManager.PERMISSION_GRANTED) {
+                        CustomTabsIntent intent = constructExtraHeadersIntent(mSession);
+                        if (intent != null) {
+                            intent.launchUrl(this, URL);
+                        }
+                        break;
+                    } else {
+                        Log.i(TAG, "PERMISSION GRANT = " + grantResults[idx]);
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private CustomTabsIntent constructExtraHeadersIntent(CustomTabsSession session) {
-        CustomTabsIntent intent = new CustomTabsIntent.Builder(session).build();
+        Log.i(TAG, "PERMISSION RESULT = " + PERMISSION);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{PERMISSION}, 0);
+            return null;
+        }
+
+        CustomTabsIntent intent = null;
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        if (session != null) {
+            builder.setSession(session);
+        } else {
+            builder.setShowTitle(false);
+            builder.setUrlBarHidingEnabled(true);
+        }
 
         // Example non-cors-whitelisted headers.
-        Bundle headers = new Bundle();
-        headers.putString("bearer-token", "Some token");
-        headers.putString("redirect-url", "Some redirect url");
-        intent.intent.putExtra(Browser.EXTRA_HEADERS, headers);
+        intent = builder.build();
+        intent.intent.putExtra(Browser.EXTRA_HEADERS, mHeaders);
+        intent.intent.putExtra(ACTION, ACTION);
+        intent.intent.putExtra(Browser.EXTRA_HEADERS + ".permission", PERMISSION);
+        if (session == null) {
+            intent.intent.putExtra(CustomTabsIntent.EXTRA_SESSION, (String) null);
+        }
 
         return intent;
     }
